a8368476ce9d523bfe5ff2f8844213c8a858f471
Lois Choi <loisch22@gmail.com>
2017-10-27 11:38:31 -0700

    change dbcontext back to before adding identity

--- MarioProducts/Models/MarioProductsDbContext.cs
+++ MarioProducts/Models/MarioProductsDbContext.cs
@@ -4,71 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace MarioProducts.Models
 {
-    public class MarioProductsDbContext : IdentityDbContext<ApplicationUser>
-    {
+	public class MarioProductsDbContext : DbContext
+	{
+		public virtual DbSet<Product> Products { get; set; }
+		public virtual DbSet<Review> Reviews { get; set; }
 
-		public MarioProductsDbContext(DbContextOptions options) : base(options)
-        {
-
-		}
-
-		protected override void OnModelCreating(ModelBuilder builder)
-		{
-			base.OnModelCreating(builder);
-
-			builder.Entity<ApplicationUser>(entity => entity.Property(m => m.Id)
-				.HasMaxLength(255));
-			builder.Entity<ApplicationUser>(entity => entity.Property(m => m.NormalizedEmail)
-				.HasMaxLength(255));
-			builder.Entity<ApplicationUser>(entity => entity.Property(m => m.NormalizedUserName)
-				.HasMaxLength(255));
-
-			builder.Entity<IdentityRole>(entity => entity.Property(m => m.Id)
-				.HasMaxLength(255));
-			builder.Entity<IdentityRole>(entity => entity.Property(m => m.NormalizedName)
-				.HasMaxLength(255));
-
-			builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.LoginProvider)
-				.HasMaxLength(255));
-			builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.ProviderKey)
-				.HasMaxLength(255));
-			builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.UserId)
-				.HasMaxLength(255));
-
-			builder.Entity<IdentityUserRole<string>>(entity => entity.Property(m => m.UserId)
-				.HasMaxLength(255));
-			builder.Entity<IdentityUserRole<string>>(entity => entity.Property(m => m.RoleId)
-				.HasMaxLength(255));
-
-			builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.UserId)
-				.HasMaxLength(255));
-			builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.LoginProvider)
-				.HasMaxLength(255));
-			builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.Name)
-				.HasMaxLength(255));
-
-			builder.Entity<IdentityUserClaim<string>>(entity => entity.Property(m => m.Id)
-				.HasMaxLength(255));
-			builder.Entity<IdentityUserClaim<string>>(entity => entity.Property(m => m.UserId)
-				.HasMaxLength(255));
-			builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property(m => m.Id)
-				.HasMaxLength(255));
-			builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property(m => m.RoleId)
-				.HasMaxLength(255));
-
-		}
-
-        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
-        public virtual DbSet<Product> Products { get; set; }
-        public virtual DbSet<Review> Reviews { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder
-            .UseMySql(@"Server=localhost;Port=8889;database=mario_products;uid=root;pwd=root;");
-    }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		=> optionsBuilder
+			.UseMySql(@"Server=localhost;Port=8889;database=mario_products;uid=root;pwd=root;");
+	}
 
 }
