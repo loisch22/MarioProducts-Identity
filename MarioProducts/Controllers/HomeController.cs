56ed1e09c12573264e8b5d9517c9aec4a43a678d
Lois Choi <loisch22@gmail.com>
2017-10-30 18:57:29 -0700

    add logic to display top 3 rated and most recent - work on styling home view

--- MarioProducts/Controllers/HomeController.cs
+++ MarioProducts/Controllers/HomeController.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Threading.Tasks;
+using MarioProducts.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MarioProducts.Controllers
 {
     public class HomeController : Controller
     {
+        private MarioProductsDbContext _db = new MarioProductsDbContext();
+
         public IActionResult Index()
         {
+            List<Product> mostRecent = _db.Products.OrderByDescending(x => x.ProductId).Take(3).ToList();
+            List<Product> bestRated = _db.Products.Include(x => x.Reviews).OrderByDescending(r => r.AverageRating()).Take(3).ToList();
+
+            ViewData["mostRecent"] = mostRecent;
+            ViewData["bestRated"] = bestRated;
+
             return View();
         }
+
     }
 }
